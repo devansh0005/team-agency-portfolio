b1a0fe7efc43b738c2012c3de2be9e30f96c918d
piyush chouhan <chouhanpiyush6002@gmail.com>
2026-06-19 16:48:52 +0530

    feat: create team data structure in app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const teamMembers = [
+    {
+        name: "Devansh",
+        github: "https://github.com/devansh0005",
+        avatar: "https://github.com/devansh0005.png",
+        skills: ["HTML", "CSS", "Vanilla JS"]
+    },
+    {
+        name: "Ashwin",
+        github: "https://github.com/ashwinlal172-creator",
+        avatar: "https://github.com/ashwinlal172-creator.png",
+        skills: ["JavaScript", "DOM Manipulation", "Git"]
+    },
+    {
+        name: "Piyush",
+        github: "https://github.com/Piy404",
+        avatar: "https://github.com/Piy404.png",
+        skills: ["HTML5", "CSS Grid", "Responsive Design"]
+    },
+    {
+        name: "Ganesh",
+        github: "https://github.com/ganeeshchoudhary",
+        avatar: "https://github.com/ganeeshchoudhary.png",
+        skills: ["Advanced JS", "Async/Await", "Web APIs"]
+    }
+];
